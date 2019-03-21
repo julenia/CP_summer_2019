28ee1736a5f7d19ff363d5549a09819b48088c64
julenia <julia.jankowska@onet.eu>
2019-03-21 10:48:06 +0100

    while loops

---
+++ src/pl/waw/sgh/Blocks.java
@@ -0,0 +1,31 @@
+package pl.waw.sgh;
+
+public class Blocks {
+
+    private static int d = 6;
+    public static void myProcedure() {
+        System.out.println(d);
+    }
+    public static int myFunc(int d) {
+        return d*10;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        //System.out.println(d);
+        {
+            int b=7;
+            System.out.println(a);
+            System.out.println(b);
+            {
+                int c = 7;
+                System.out.println(a);
+                System.out.println(b);
+            }
+            //System.out.println(c);
+        }
+        System.out.println(a);
+        //System.out.println(b);
+        }
+    }
+
---
+++ src/pl/waw/sgh/Test.java
@@ -0,0 +1,7 @@
+package pl.waw.sgh;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
