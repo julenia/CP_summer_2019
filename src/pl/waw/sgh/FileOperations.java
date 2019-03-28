89003f420a79de6121b1561bd21f71774f160e54
jj87319 <julia.jankowska@onet.eu>
2019-03-28 12:57:32 +0100

    file operations

--- src/pl/waw/sgh/FileOperations.java
+++ src/pl/waw/sgh/FileOperations.java
@@ -1,13 +1,30 @@
 package src.pl.waw.sgh;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileOperations {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String path = "myfile.txt";
         File file = new File(path);
+        String cwd = System.getProperty("user.dir");
 
         String temp = System.getenv("TEMP");
         System.out.println(temp);
+        System.out.println(System.getProperty("java.io.tmpdir"));
+        System.out.println(cwd);
+
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            System.out.println(line);
+        }
+
+        System.out.println(file.length());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getCanonicalPath());
+        System.out.println(file.getParentFile());
+
     }
 }
