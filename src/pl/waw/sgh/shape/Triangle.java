1acffbcf44f9a5bf72eb2e96a0b100fe52657ade
Julia Jankowska <48432623+julenia@users.noreply.github.com>
2019-04-18 10:16:55 +0200

    Add files via upload

---
+++ src/pl/waw/sgh/shape/Triangle.java
@@ -0,0 +1,23 @@
+package pl.waw.sgh.shape;
+
+public class Triangle extends shape{
+    public Triangle (double parA, double parB){
+        super(parA, parB);
+    }
+    public Triangle(double parA){
+        super(parA);
+        parB=0.5*parA*Math.sqrt(2);
+    }
+
+    public double calcSurface() {
+        return 0.5*parA*parB;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "parA=" + parA +
+                ", parB=" + parB +
+                '}';
+    }
+}
