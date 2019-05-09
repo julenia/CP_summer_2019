c42572b9204486906010f5c5ed57f9e6192aa97b
jj87319 <julia.jankowska@onet.eu>
2019-05-09 12:34:24 +0200

    First Window + button action

---
+++ src/pl/waw/sgh/bank/ui/FirstWindow.java
@@ -0,0 +1,37 @@
+package src.pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FirstWindow {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("First Window");
+        frame.setSize(800, 500 );
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        LayoutManager layoutManager=new FlowLayout();
+        frame.setLayout(layoutManager);
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(new JLabel("Hello, what's your name: "));
+        JTextField nameField = new JTextField(50);
+        mainPanel.add(nameField);
+        JButton okButton= new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Hello "+nameField.getText());
+            }
+        });
+
+        mainPanel.add(okButton);
+        frame.add(mainPanel);
+
+
+        frame.setVisible(true);
+    }
+}
