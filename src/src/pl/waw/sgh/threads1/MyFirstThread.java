a4bb89359eceddc524b0db494b237ad09ea803c6
jj87319 <julia.jankowska@onet.eu>
2019-05-23 11:27:10 +0200

    first threads

---
+++ src/src/pl/waw/sgh/threads1/MyFirstThread.java
@@ -0,0 +1,29 @@
+package src.src.pl.waw.sgh.threads1;
+
+public class MyFirstThread extends Thread{
+
+    private String name;
+    private int iterator;
+    private Thread prevThread;
+    private AllThreads allThreads;
+
+    public MyFirstThread(String name, AllThreads allThreads){
+        this.name = name;
+        this.allThreads=allThreads;
+    }
+
+
+    @Override
+    public void run() {
+        while (iterator<10) {
+            try {
+                iterator++;
+                allThreads.increase(name);
+                System.out.println(name + " it= " + iterator);
+                this.sleep(800);
+            }catch (InterruptedException ie) {
+                System.out.println("Interrupted "+ name + " at: " + iterator);
+            }
+        }
+    }
+}
